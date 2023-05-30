7e04202f3c530ab135a773bb26cbb2dd9d1c6e33
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-05-30 22:22:48 +0530

    Html file and image resourses added

---
+++ javascript.js
@@ -0,0 +1,65 @@
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
+
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 2000); // Change image every 2 seconds
+} 
+
+function zoomin(){
+    var myImg = document.getElementById("sky");
+    var currWidth = myImg.clientWidth;
+    if(currWidth == 500){
+        alert("Maximum zoom-in level reached.");
+    } else{
+        myImg.style.width = (currWidth + 50) + "px";
+    } 
+}
+function zoomout(){
+    var myImg = document.getElementById("sky");
+    var currWidth = myImg.clientWidth;
+    if(currWidth == 50){
+        alert("Maximum zoom-out level reached.");
+    } else{
+        myImg.style.width = (currWidth - 50) + "px";
+    }
+}
+
+
+
